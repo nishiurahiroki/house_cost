a8a7076de4e0e6b6183478e408e567294a74536c
nishiura hiroki <nishiurahiroki@gmail.com>
2019-01-17 11:52:30 -0500

    join type tables.

--- src/repositories/CostRepository.js
+++ src/repositories/CostRepository.js
@@ -21,11 +21,32 @@ export default class CostRepository {
   }
 
   static async getCosts({year = '', month = '', day = '', costId = '', userId}) {
+    const costTypes = await firebaseInstance
+      .database()
+      .ref(`costTypes/${userId}`)
+      .once('value')
+      .then(snapshot => snapshot.val() ? snapshot.val() : [])
+      .then(datas => Object.entries(datas).map(([key, value]) => ({key, ...value})))
+
     return await firebaseInstance
       .database()
       .ref(`costs/${userId}`)
       .once('value')
-      .then(snapshot => snapshot.val() ? snapshot.val() : [])
-      .then(datas => Object.entries(datas).map(([key, value]) => ({key, ...value})))
+      .then(snapshot => {
+        const costs = snapshot.val()
+        if(!costs) {
+          return []
+        }
+
+        return Object
+          .entries(costs)
+          .map(([key, value]) => ({key, ...value}))
+          .map(cost => {
+            return {
+              ...cost,
+              name : costTypes.find(type => cost.id == type.id).name
+            }
+          })
+      })
   }
 }
--- src/repositories/IncomeRepository.js
+++ src/repositories/IncomeRepository.js
@@ -21,11 +21,32 @@ export default class IncomeRepository {
   }
 
   static async getIncomes({year = '', month = '', day = '', incomeId = '', userId}) {
+    const incomeTypes = await firebaseInstance
+      .database()
+      .ref(`incomeTypes/${userId}`)
+      .once('value')
+      .then(snapshot => snapshot.val() ? snapshot.val() : [])
+      .then(datas => Object.entries(datas).map(([key, value]) => ({key, ...value})))
+
     return await firebaseInstance
       .database()
       .ref(`incomes/${userId}`)
       .once('value')
-      .then(snapshot => snapshot.val() ? snapshot.val() : [])
-      .then(datas => Object.entries(datas).map(([key, value]) => ({key, ...value})))
+      .then(snapshot => {
+        const incomes = snapshot.val()
+        if(!incomes) {
+          return []
+        }
+
+        return Object
+          .entries(incomes)
+          .map(([key, value]) => ({key, ...value}))
+          .map(cost => {
+            return {
+              ...cost,
+              name : incomeTypes.find(type => cost.id == type.id).name
+            }
+          })
+      })
   }
 }
